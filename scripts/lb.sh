cd005b3fed1c4fa217f7654e4021c08c276a695b
BlueTeleco <lucas.mt.l@protonmail.com>
2018-01-21 23:53:50 +0100

    Changed load balancer algorithm

--- scripts/lb.sh
+++ scripts/lb.sh
@@ -18,6 +18,6 @@ do
 	back="$back --backend 10.1.3.1$n:3000"
 done
 
-xterm -hold -e "sudo lxc-attach --clear-env -n lb -- xr --verbose --server tcp:0:80 -dr $back --web-interface 0:8001" &
+xterm -hold -e "sudo lxc-attach --clear-env -n lb -- xr --verbose --server tcp:0:80 -d lax-stored-ip:60 $back --web-interface 0:8001" &
 echo 'Load balancer up and running'
 echo
